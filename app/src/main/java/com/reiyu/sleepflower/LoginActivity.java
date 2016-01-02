0d0860d58eb7ae4ba56043db95a8a638941992f6
rei___yu <satomi.i.sakura@gmail.com>
2016-01-02 14:48:18 +0900

    store sign_in info in shared preference

--- app/src/main/java/com/reiyu/sleepflower/LoginActivity.java
+++ app/src/main/java/com/reiyu/sleepflower/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.reiyu.sleepflower;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
@@ -50,5 +52,8 @@ public class LoginActivity extends FragmentActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.edit().putBoolean("@string/sign_in", true).commit();
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
     }
 }
--- app/src/main/java/com/reiyu/sleepflower/MainActivity.java
+++ app/src/main/java/com/reiyu/sleepflower/MainActivity.java
@@ -1,7 +1,9 @@
 package com.reiyu.sleepflower;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -28,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!(sp.getBoolean("@string/sign_in", false))) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 
     @Override
